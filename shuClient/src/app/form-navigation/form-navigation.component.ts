bedc38f651587821c66360d022509f4d75c43e64
Faruk <gehafar@gmail.com>
2018-12-09 13:03:48 +0300

    Add navigation component

---
+++ shuClient/src/app/form-navigation/form-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-navigation',
+  templateUrl: './form-navigation.component.html',
+  styleUrls: ['./form-navigation.component.css']
+})
+export class FormNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
