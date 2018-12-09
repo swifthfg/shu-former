c62400d5aa315e0406bdc305baf151ba551b8ec3
Faruk <gehafar@gmail.com>
2018-12-09 14:12:52 +0300

    Add angular material

--- shuClient/src/app/app.module.ts
+++ shuClient/src/app/app.module.ts
@@ -4,16 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RestService } from './rest.service';
 import { FormNavigationComponent } from './form-navigation/form-navigation.component';
+import { DataService } from "./data.service";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { FormQuestionsComponent } from './form-questions/form-questions.component';
+import { FormResultComponent } from './form-result/form-result.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatRadioModule} from '@angular/material';
+
+const appRoutes: Routes = [
+  {path: 'form', component: FormQuestionsComponent},
+  {path: 'form-result', component: FormResultComponent},
+  {path: '**', redirectTo: 'form'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormNavigationComponent
+    FormNavigationComponent,
+    FormQuestionsComponent,
+    FormResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [RestService],
+  providers: [RestService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
