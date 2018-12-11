95d9afec2e1731ed7d17e0b38eb9b68210fbce9d
faruk <gehafar@gmail.com>
2018-12-11 10:13:50 +0300

    Add data service to navigation component

--- shuClient/src/app/form-navigation/form-navigation.component.ts
+++ shuClient/src/app/form-navigation/form-navigation.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, DoCheck, OnInit} from '@angular/core';
+import {DataService} from "../data.service";
 
 @Component({
   selector: 'app-form-navigation',
   templateUrl: './form-navigation.component.html',
   styleUrls: ['./form-navigation.component.css']
 })
-export class FormNavigationComponent implements OnInit {
-
-  constructor() { }
+export class FormNavigationComponent implements OnInit, DoCheck {
+  showQuestionButton = true;
+  constructor(private data: DataService) { }
 
   ngOnInit() {
+    this.showQuestionButton = !this.data.isUserSubmittedFormBefore();
+  }
+
+  ngDoCheck() {
+    this.showQuestionButton = !this.data.isUserSubmittedFormBefore();
   }
 
 }
